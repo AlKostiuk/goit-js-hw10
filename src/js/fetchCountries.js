3a7fecde4eddc1c9b0a884585999d048bbd168f4
AlKostiuk <Kostiuk.A96@gmail.com>
2023-04-22 18:26:14 +0300

    first commit

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,9 @@
+function fetchCountries(countryName) {
+  return fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+      return response.json();
+    })
+    .catch(error => console.error(error));
+}
+
+export { fetchCountries };
