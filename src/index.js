eac1adb36083058e209263237074a348c7fc96eb
AlKostiuk <Kostiuk.A96@gmail.com>
2023-04-22 18:29:46 +0300

    full homework

--- src/index.js
+++ src/index.js
@@ -1,70 +1,75 @@
 import './css/styles.css';
-import {fetchCountries} from './js/fetchCountries'
+import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 
-const inputCountryName = document.querySelector("#search-box");
-const countryInfo = document.querySelector('.country-info')
-const countryList = document.querySelector('.country-list')
+const inputCountryName = document.querySelector('#search-box');
+const countryInfo = document.querySelector('.country-info');
+const countryList = document.querySelector('.country-list');
 
-inputCountryName.addEventListener('input', debounce(handleInputSearch , DEBOUNCE_DELAY))
+inputCountryName.addEventListener(
+  'input',
+  debounce(handleInputSearch, DEBOUNCE_DELAY)
+);
 
-
-function countryMarkup(elements){
+function countryMarkup(elements) {
   const resultMarkup = elements
-  .map((element) => {
-    return `<li class = country_search>
+    .map(element => {
+      return `<li class = country_search>
         <img class = country_flag src= "${element.flags.svg}"/>
         <h2 class = country_name >${element.name.official} </h2>
     </li>`;
-  })
-  .join("");
+    })
+    .join('');
   return resultMarkup;
 }
 
-function countryElementInfo(elements){
+function countryElementInfo(elements) {
   const resultElementInfo = elements
-  .map((element) => {
-    return `<div class = country_info_flex>
+    .map(element => {
+      return `<div class = country_info_flex>
     <img class = one_country_flag src= "${element.flags.svg}"/>
         <h1 class = one_country_name >${element.name.official} </h2>
 
         </div>
-        <p class = country_capital ><strong> Capital:</strong> ${element.capital} </p>
-        <p class = country_population > <strong> Population:</strong>${element.population} </p>
-        <p class = country_language ><strong> Languages:</strong> ${Object.values(element.languages).join(",")} </p>`
-
-  })
-  .join("");
-  return resultElementInfo
+        <p class = country_capital ><strong> Capital:</strong> ${
+          element.capital
+        } </p>
+        <p class = country_population > <strong> Population:</strong>${
+          element.population
+        } </p>
+        <p class = country_language ><strong> Languages:</strong> ${Object.values(
+          element.languages
+        ).join(',')} </p>`;
+    })
+    .join('');
+  return resultElementInfo;
 }
 
-
-
-
-function handleInputSearch(){
+function handleInputSearch() {
   let curentCountry = inputCountryName.value.trim();
-  if(curentCountry === ""){
-    return
+  if (curentCountry === '') {
+    return;
   }
   fetchCountries(curentCountry)
-  .then(countries => {
-    countryInfo.innerHTML = ""
-    countryList.innerHTML = ""
-    if(countries.length > 10){
-      Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
-    }else if(countries.length ===1){
-      const markupCountryElement =countryElementInfo(countries)
-      countryInfo.insertAdjacentHTML('beforeend', markupCountryElement)
-    }else {
-      const listMarkup = countryMarkup(countries)
-      countryList.insertAdjacentHTML('beforeend', listMarkup)
-    }
-  }).catch(()=>{
-    Notiflix.Notify.failure("Oops, there is no country with that name")
-  })
-
+    .then(countries => {
+      countryInfo.innerHTML = '';
+      countryList.innerHTML = '';
+      if (countries.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (countries.length === 1) {
+        const markupCountryElement = countryElementInfo(countries);
+        countryInfo.insertAdjacentHTML('beforeend', markupCountryElement);
+      } else {
+        const listMarkup = countryMarkup(countries);
+        countryList.insertAdjacentHTML('beforeend', listMarkup);
+      }
+    })
+    .catch(() => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
 }
-
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,11 @@
 function fetchCountries(countryName) {
-  return fetch(`https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`)
+  return fetch(
+    `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`
+  )
     .then(response => {
+      if (response.status === 404) {
+        throw new Error('country not found');
+      }
       return response.json();
     })
     .catch(error => console.error(error));
